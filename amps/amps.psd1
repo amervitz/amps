6d198109e8687cac7d6318b9bec3111be955eefc
Alan Mervitz <amervitz@users.noreply.github.com>
2017-02-06 01:33:17 -0600

    Enter-Object (with) function (#1)
    Add the Enter-Object function for accessing the properties of an object or items of a hashtable as variables within a new scope without needing to refer to the containing object. This makes it easy to splat a nested object without temporary variable assignments.
    
    It has an alias of 'with' to provide a shorthand version that makes it feel like a built-in language construct.

---
+++ amps/amps.psd1
@@ -0,0 +1,58 @@
+﻿@{
+
+# Script module or binary module file associated with this manifest.
+RootModule = 'amps.psm1'
+
+# Version number of this module.
+ModuleVersion = '0.1.0'
+
+# ID used to uniquely identify this module
+GUID = '11844c8e-40ec-41eb-8c05-9b7e494bf6ea'
+
+# Author of this module
+Author = 'Alan Mervitz'
+
+# Copyright statement for this module
+Copyright = '(c) 2017 Alan Mervitz. All rights reserved.'
+
+# Description of the functionality provided by this module
+Description = 'A PowerShell script module with advanced functions that extend PowerShell.'
+
+# Minimum version of the Windows PowerShell engine required by this module
+PowerShellVersion = '5.0'
+
+# Functions to export from this module
+FunctionsToExport = 'Enter-Object'
+
+# Aliases to export from this module
+AliasesToExport = 'with'
+
+# Private data to pass to the module specified in RootModule/ModuleToProcess. This may also contain a PSData hashtable with additional module metadata used by PowerShell.
+PrivateData = @{
+
+    PSData = @{
+
+        # Tags applied to this module. These help with module discovery in online galleries.
+        Tags = @('splat')
+
+        # A URL to the license for this module.
+        LicenseUri = 'https://github.com/amervitz/amps/blob/master/LICENSE'
+
+        # A URL to the main website for this project.
+        ProjectUri = 'https://github.com/amervitz/amps'
+
+        # ReleaseNotes of this module
+        ReleaseNotes = @"
+Initial release.
+
+Add the Enter-Object function for accessing the properties of an object or items of a hashtable as variables
+within a new scope without needing to refer to the containing object. This makes it easy to splat a nested 
+object without temporary variable assignments.
+
+It has an alias of 'with' to provide a shorthand version that makes it feel like a built-in language construct.
+"@
+    } # End of PSData hashtable
+
+} # End of PrivateData hashtable
+
+}
---
+++ amps/amps.psm1
@@ -0,0 +1,91 @@
+﻿Set-StrictMode -Version Latest
+
+<#
+.SYNOPSIS
+   Enter-Object destructures an object and makes its properties available as variables in a new scope.
+   
+.DESCRIPTION
+   Enter-Object makes available as variables the properties of an object, or the items of a hashtable,
+   within a new scope without needing to refer to the object itself. It has an alias of 'with' to provide 
+   a shorthand version that makes it feel like a built-in language construct.
+
+   The most common anticipated use of this function is to perform splatting operations on a nested object's 
+   properties or nested hashtable's items without having to perform temporary variable assignments to refer
+   to the nested properties/items.
+
+   Enter-Object removes the use of temporary variables and becomes more concise when dealing with complex 
+   configuration objects where multiple splatting operations are needed.
+
+.EXAMPLE
+   >
+   $loginResult = @{
+       Success = $false
+       Reason = 'Expired'
+       Message = @{
+           Object = "Your password has expired, please reset your password."
+           BackgroundColor = "Red"
+           ForegroundColor = "Yellow"
+       }
+   }
+
+   Enter-Object $loginResult {
+       Write-Host @Message
+   }
+
+   This example shows how a nested hashtable can be splatted directly without having to assign it to a new
+   temporary variable as would normally be needed as follows:
+
+   $message = $loginResult.Message
+   Write-Host @message
+
+.EXAMPLE
+   >
+   with $loginResult {
+       Write-Host @Message
+   }
+
+   Building upon the first example, this shows the use of the 'with' alias which makes this feel like a
+   built-in keyword. The 'with' alias was chosen because it's a keyword found in the VB.NET language that 
+   performs the same purpose.
+
+.OUTPUTS
+   Any objects written to the output stream will be returned.
+#>
+function Enter-Object {
+    [CmdletBinding()]
+    [Alias("with")]
+    param (
+        # The object or hashtable to destructure and make available as variables within the script block.
+        # When supplying an object the properties will be exposed as the variables.
+        # When supplying a hashtable the items will be exposed as the variables.
+        # Only the first-level of properties/items from the input object are converted into variables.
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [ValidateNotNull()]
+        [Object[]]
+        $InputObject,
+
+        # The script block with commands to execute in a new nested scope with access to the variables
+        # from the decomposed input object.
+        [Parameter(Mandatory)]
+        [ValidateNotNull()]
+        [ScriptBlock]
+        $ScriptBlock
+    )
+    process
+    {
+        foreach($io in $InputObject) {
+            Write-Verbose "InputObject type: $($io.GetType().FullName)"
+
+            $variables = if($io -is [Hashtable]) {
+                $io.Keys | ForEach-Object { [PSVariable]::new($_,$io.Item($_)) }
+            } else {
+                $io.PSObject.Properties | ForEach-Object { [PSVariable]::new($_.Name,$_.Value) }
+            }
+
+            Write-Verbose "ScriptBlock variables: $($variables.Name -join ',')"
+
+            # functions, variables, arguments
+            $ScriptBlock.InvokeWithContext($null,[PSVariable[]]$variables,$null)
+        }
+    }
+}
